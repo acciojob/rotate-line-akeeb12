1544ced6c6b067773f884aec50e752905e051cc5
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-31 20:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
 let angle = 0;
 let line = document.getElementById("line");
 
 setInterval(() =>{
 	angle += 2;
-	line.style.transform = "translate(-50%, -50%)rotate (" + angle + deg")";
+	line.style.transform = "translate(-50%, -50%) rotate(" + angle + "deg)";
 }, 20);
